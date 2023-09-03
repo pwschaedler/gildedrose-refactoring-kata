ee154d2a40f273262ffc283f4bd0c158173e740b
Peter Schaedler <pwschaedler@gmail.com>
2023-09-03 13:45:29 -0700

    Single quotes

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -5,7 +5,7 @@ from gilded_rose import GildedRose, Item
 
 def test_foo() -> None:
     """Placeholder test."""
-    items = [Item("foo", 0, 0)]
+    items = [Item('foo', 0, 0)]
     gilded_rose = GildedRose(items)
     gilded_rose.update_quality()
-    assert "foo" == items[0].name
+    assert 'foo' == items[0].name
