55ef1a575461f984cd9c229489cfc9cad86b5c45
Peter Schaedler <pwschaedler@gmail.com>
2023-09-03 13:50:02 -0700

    Add test for quality never going above 50

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -29,3 +29,11 @@ def test_aged_brie_increases_quality() -> None:
     # Past its sell by date, it increases in quality by 2
     gilded_rose.update_quality()
     assert items[0].quality == 3
+
+
+def test_quality_never_more_than_50() -> None:
+    """The Quality of an item is never more than 50."""
+    items = [Item('Aged Brie', 0, 50)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].quality == 50
